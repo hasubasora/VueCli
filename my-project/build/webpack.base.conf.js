ba5835cc4917fe48dd61bb9a2a7e024aff0f217d
chenyongh <chenyongh@sunaw.net>
2017-06-26 18:06:14 +0800

    SASS安装完毕

--- my-project/build/webpack.base.conf.js
+++ my-project/build/webpack.base.conf.js
@@ -3,56 +3,92 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
 }
+//提取到专用文件中
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-  entry: {
-    app: './src/main.js'
-  },
-  output: {
-    path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+    entry: { // 配置入口
+        app: './src/main.js'
+    },
+    output: { // 编译后的文件路径
+        path: config.build.assetsRoot,
+        filename: '[name].js',
+        publicPath: process.env.NODE_ENV === 'production' ?
+            config.build.assetsPublicPath : config.dev.assetsPublicPath
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src')
         }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
-    ]
-  }
+    },
+    module: { // 编译规则
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: vueLoaderConfig
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [resolve('src'), resolve('test')]
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+
+            //解析.scss文件,对于用 import 或 require 引入的sass文件进行加载，以及<style lang="sass">...</style>声明的内部样式进行加载
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development 
+                    fallback: "style-loader"
+                })
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                }
+            }
+        ]
+    },
+    plugins: [ // 辅助的插件
+        extractSass,
+        new ExtractTextPlugin("style.css") //提取出来的样式放在style.css文件中
+    ],
+    // //.vue文件的配置，以下是为了在.vue文件中使用ES6语法（必须安装babel相关的依赖包），以及把使用css或sass语法的样式提取出来，如果不需要可以忽略
+    // vue: {
+    //     loaders: {
+    //         js: 'babel',
+    //         css: ExtractTextPlugin.extract("css"),
+    //         sass: ExtractTextPlugin.extract("css!sass")
+    //     },
+    // },
+
 }
--- my-project/src/components/hello.vue
+++ my-project/src/components/hello.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
-    {{msg}}
-  </div>
+
+   <li>   1 {{msg}}</li> <br><li>222222222</li>
+  </div> 
 </template>
 
 <script>
@@ -9,14 +10,21 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: '哈哈哈哈哈哈'
+      msg: '移动11我11',
+      methods:{
+        yd:function(){
+          
+        }
+      }
     }
   }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h1, h2 {
   font-weight: normal;
 }
@@ -24,11 +32,15 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
+  
 }
-
+.hello{
+  border: 1px red solid;
+}
 li {
   display: inline-block;
   margin: 0 10px;
+  @extend .hello;
 }
 
 a {
